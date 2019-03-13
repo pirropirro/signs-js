9f54288441499e54f5b7d32700840206965b0a3b
pirropirro <fra.pirrotta@gmail.com>
2019-03-13 17:55:43 +0100

    Fix exports

--- scripts/bootstrapper/Application.ts
+++ scripts/bootstrapper/Application.ts
@@ -1,16 +1,15 @@
 import { forEach } from "lodash";
 import { Container } from "inversify";
-import getDecorators from "inversify-inject-decorators";
 
 import { IModule } from "./IModule";
 import { ControllerUtil } from "./Route";
 import { IExpress } from "../engine/IExpress";
 import { MainModule } from "../modules/MainModule";
-import { IMiddleware } from '../engine/IMiddleware';
+import { IMiddleware } from '../middlewares/IMiddleware';
+import { IExpressConfig } from "../engine/IExpressConfig";
 import { IController, IHandlerFactory } from '../engine/IController';
 
 let container = new Container();
-export let { lazyInject, lazyMultiInject } = getDecorators(container);
 
 export class Application {
     protected container = container;
@@ -26,11 +25,12 @@ export class Application {
     run() {
         let exp = this.container.get<IExpress>("IExpress");
         let factory = this.container.get<IHandlerFactory>("IHandlerFactory");
+        let { suffix = "api" } = this.container.get<IExpressConfig>("IExpressConfig");
 
         this.container.getAll<IMiddleware>("IMiddleware").forEach(m => exp.app.use(m.transform.bind(m)));
         this.container.getAll<IController>("IController").forEach(c => {
             let { base, routes } = ControllerUtil.getMetadata(c.constructor);
-            forEach(routes, (metadata => exp.app[metadata.type](`/api${base}${metadata.location}`, factory.create(c, metadata))));
+            forEach(routes, (metadata => exp.app[metadata.type](`/${suffix}${base}${metadata.location}`, factory.create(c, metadata))));
         });
 
         exp.start();
